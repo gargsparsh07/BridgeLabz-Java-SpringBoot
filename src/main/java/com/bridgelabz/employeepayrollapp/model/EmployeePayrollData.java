33d5fd295ee47e99bbe20917d432dc35c8090ceb
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-04-10 16:15:13 +0530

    [Sparsh]: UC1: Introduced DTO and Model to Employee Payroll App

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
